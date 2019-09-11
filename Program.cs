fe238c472a79462d676e865baa3a8a0ae9570f57
karinapodymova1 <karinapodymova1@gmail.com>
2019-09-10 21:04:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PodymovaClassWeek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years of experience do you have in professional programming?");
+            var years = Console.ReadLine();
+            int years1 = int.Parse(years);
+            var count = 0;
+
+            for (int i = 1; i <= years1; i++)
+            {
+                Console.WriteLine("You have " + i + " years of experience");
+                count++;
+            }
+        }
+    }
+}
